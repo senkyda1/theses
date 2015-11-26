857dda7a6d1762655242008d25a1e3adbcbc7c82
senkyda1 <senkyda1@fit.cvut.cz>
2015-11-26 14:41:30 +0100

    Dokonceno.

---
+++ app/models/thesis.rb
@@ -0,0 +1,7 @@
+class Thesis < ActiveRecord::Base
+  belongs_to :faculty
+  belongs_to :person
+
+  validates :title, presence: true
+  validates :abstract, presence: true
+end
---
+++ app/views/people/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @person, :id, :username, :name, :surname, :age, :created_at, :updated_at
